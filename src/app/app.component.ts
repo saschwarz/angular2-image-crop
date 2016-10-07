f6e9673cf128b117fdccf2464ddd96e3af1a869c
Steve Schwarz <steve@agilitynerd.com>
2016-10-07 18:33:43 -0500

    Size mask and translate background image.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ImageCropComponent } from './image-crop/image-crop.component';
 
 @Component({
   selector: 'app-root',
--- src/app/image-crop/image-crop.component.ts
+++ src/app/image-crop/image-crop.component.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer, SafeStyle} from '@angular/platform-browser';
+
+class Mask {
+  width: Number;
+  height: Number;
+}
 
 @Component({
-  selector: 'image-crop',
+  selector: 'app-image-crop',
   templateUrl: './image-crop.component.html',
   styleUrls: ['./image-crop.component.css']
 })
 export class ImageCropComponent implements OnInit {
+  image: SafeStyle;
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+  rotation: number;
 
-  constructor() { }
+  constructor(sanitizer: DomSanitizer) {
+    this.image = sanitizer.bypassSecurityTrustStyle('url(' + '/assets/course.png' + ')');
+    this.width = 100;
+    this.height = 200;
+    this.x = 0;
+    this.y = 0;
+    this.rotation = 0;
+  }
+
+  public getStyles(){
+    return {
+      'background-position': this.x + 'px ' + this.y + 'px',
+      'height': this.height + 'px',
+      'width': this.width + 'px'
+    }
+  }
 
   ngOnInit() {
   }
