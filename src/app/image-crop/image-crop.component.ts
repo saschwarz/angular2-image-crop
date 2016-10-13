abad48a86b89f416e2bbc571a169ea6d56babc25
Steve Schwarz <steve@agilitynerd.com>
2016-10-13 15:16:11 -0500

    Add panning

--- src/app/image-crop/image-crop.component.ts
+++ src/app/image-crop/image-crop.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {DomSanitizer, SafeStyle} from '@angular/platform-browser';
+import 'hammerjs';
 
 
 @Component({
@@ -15,9 +16,15 @@ export class ImageCropComponent implements OnInit {
   @Input() maskHeight: number;
   @Input() rotation: number = 0;
 
-  private x: number = 0; // noqa
+  /* tslint:disable */
+  private x: number = 0;
   private y: number = 0;
+  /* tslint:enable */
   private image: SafeStyle;
+
+  /* pan/rotate event book keeping */
+  private startX: number = 0;
+  private startY: number = 0;
 
   constructor(private sanitizer: DomSanitizer) {
   }
@@ -36,6 +43,25 @@ export class ImageCropComponent implements OnInit {
     };
   }
 
+  onRotate(event: any /** TODO #9100 */): void {
+    event.preventDefault();
+    this.rotation = event.rotation;
+  }
+
+  onPanStart(event: any): void {
+    this.startX = this.x;
+    this.startY = this.y;
+  }
+
+  onPan(event: any /** TODO #9100 */): void {
+    event.preventDefault();
+    this.x = this.startX + event.deltaX;
+    this.y = this.startY + event.deltaY;
+  }
+
+  public onKey(event): void {
+    console.log(event.key);
+  }
   ngOnInit() {
     this.image = this.sanitizer.bypassSecurityTrustStyle('url(' + this.imageURL + ')');
   }
