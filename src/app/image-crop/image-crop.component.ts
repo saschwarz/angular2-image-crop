86ac4f1f9f8dddce02a5bc460633ad8b3c610f54
Steve Schwarz <steve@agilitynerd.com>
2016-10-14 20:10:20 -0500

    Keyboard rotation and image focus on load.

--- src/app/image-crop/image-crop.component.ts
+++ src/app/image-crop/image-crop.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, OnInit, Renderer, ViewChild} from '@angular/core';
 import {DomSanitizer, SafeStyle} from '@angular/platform-browser';
 import 'hammerjs';
 
@@ -8,7 +8,15 @@ import 'hammerjs';
   templateUrl: './image-crop.component.html',
   styleUrls: ['./image-crop.component.css']
 })
-export class ImageCropComponent implements OnInit {
+export class ImageCropComponent implements OnInit, AfterViewInit {
+  private image: SafeStyle;
+  /* pan/rotate event book keeping */
+  private startX: number;
+  private startY: number;
+  private startRotation: number;
+
+  @ViewChild('display') display: ElementRef;
+
   @Input() imageURL: string;
   @Input() imageWidth: number;
   @Input() imageHeight: number;
@@ -18,14 +26,36 @@ export class ImageCropComponent implements OnInit {
   @Input() x: number = 0;
   @Input() y: number = 0;
 
-  private image: SafeStyle;
+  @HostListener('keyup', ['$event'])
+  onKey(event: KeyboardEvent): void{
+      if (event.srcElement && event.srcElement.className === 'display') {
+        event.stopPropagation();
+        event.preventDefault();
+        let delta = event.shiftKey ? 10 : 1;
+        switch(event.code) {
+          case 'ArrowLeft':
+            this.x -= delta;
+            break;
+          case 'ArrowRight':
+            this.x += delta;
+            break;
+          case 'ArrowDown':
+            this.y += delta;
+            break;
+          case 'ArrowUp':
+            this.y -= delta;
+            break;
+          case 'Equal':
+            this.rotation += delta;
+            break;
+          case 'Minus':
+            this.rotation -= delta;
+            break;
+        }
+      }
+  }
 
-  /* pan/rotate event book keeping */
-  private startX: number;
-  private startY: number;
-  private startRotation: number;
-
-  constructor(private sanitizer: DomSanitizer) {
+  constructor(private sanitizer: DomSanitizer, private renderer: Renderer) {
   }
 
   public getMaskStyles() {
@@ -40,7 +70,7 @@ export class ImageCropComponent implements OnInit {
     event.preventDefault();
   }
 
-  onRotate(event: any /** TODO #9100 */): void {
+  onRotate(event: any): void {
     event.preventDefault();
     this.rotation = Math.floor((event.rotation - this.startRotation)) % 360;
   }
@@ -58,12 +88,11 @@ export class ImageCropComponent implements OnInit {
     this.y = this.startY + event.deltaY;
   }
 
-  public onKey(event): void {
-    console.log(event.key);
-  }
-
-  ngOnInit() {
+  ngOnInit(): void {
     this.image = this.sanitizer.bypassSecurityTrustStyle('url(' + this.imageURL + ')');
   }
 
+  ngAfterViewInit(): void {
+    this.renderer.invokeElementMethod(this.display.nativeElement, 'focus', []);
+  }
 }
