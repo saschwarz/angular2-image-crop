fe0f5e947efc3948c2e3dcf6e5f9b9a85c5e8438
Steve Schwarz <steve@agilitynerd.com>
2016-10-17 20:40:23 -0500

    Pan rotation resizes, rotation indicator.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular2 Image Crop UI Experiment';
+  title = 'Angular 2 Image Crop UI Experiment';
 }
