ba3cfa0b0e88d6378b4234c7e743e49318c0b1d5
Steve Schwarz <steve@agilitynerd.com>
2016-10-24 08:20:58 -0500

    new gh-pages version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ImageCropComponent } from './image-crop/image-crop.component';
-import { DimensionedImageCropComponent } from './dimensioned-image-crop/dimensioned-image-crop.component';
-import { CroppedImageComponent } from './cropped-image/cropped-image.component';
-import { DimensionedCroppedImageComponent } from './dimensioned-cropped-image/dimensioned-cropped-image.component';
+// import { ImageCropComponent } from './image-crop/image-crop.component';
+// import { DimensionedImageCropComponent } from './dimensioned-image-crop/dimensioned-image-crop.component';
+// import { CroppedImageComponent } from './cropped-image/cropped-image.component';
+// import { DimensionedCroppedImageComponent } from './dimensioned-cropped-image/dimensioned-cropped-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageCropComponent,
-    DimensionedImageCropComponent,
-    CroppedImageComponent,
-    DimensionedCroppedImageComponent
+    // ImageCropComponent,
+    // DimensionedImageCropComponent,
+    // CroppedImageComponent,
+    // DimensionedCroppedImageComponent
   ],
   imports: [
     BrowserModule,
