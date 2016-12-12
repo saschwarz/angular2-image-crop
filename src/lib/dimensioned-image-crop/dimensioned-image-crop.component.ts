70558e6c5cce047591c99bd80aa23078bd4f5324
Steve Schwarz <steve@agilitynerd.com>
2016-12-12 10:30:34 -0600

    DimensionedImageCrop responsive

--- src/lib/dimensioned-image-crop/dimensioned-image-crop.component.ts
+++ src/lib/dimensioned-image-crop/dimensioned-image-crop.component.ts
@@ -1,45 +1,28 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { DimensionedImage } from '../models';
 import { ImageCropComponent } from '../image-crop/image-crop.component';
 
 /**
- * Given a display width/height in pixels the mask and image are
- * displayed at the same scale. The displayed size of the component
- * is responsive or can fixed by specified by suplying width/height
- * attributes and the displayed size of the mask/image are then
- * calculated to match.
+ * The displayed size of the component is responsive.
  */
 @Component({
   selector: 'dimensioned-image-crop',
   templateUrl: './dimensioned-image-crop.component.html',
-  styleUrls: ['./dimensioned-image-crop.component.css'],
-  // entryComponents: [ImageCropComponent]
+  styleUrls: ['./dimensioned-image-crop.component.css']
 })
-export class DimensionedImageCropComponent implements AfterViewInit {
+export class DimensionedImageCropComponent implements AfterViewChecked {
   // Set the size of the mask in units instead of in pixels.
   //
   @Input() mask: DimensionedImage;
   @Input() image: DimensionedImage;
-  // /**
-  //  * Optional width of component.
-  //  */
-  // @Input() width: number;
-  // /**
-  //  * Optional height of the component.
-  //  */
-  // @Input() height: number;
-  // /**
-  //  * Set width/height to mask's width/height. Otherwise
-  //  * sets width/height to image's width/height.
-  //  */
-  // @Input() useMask: boolean = false;
+  @ViewChild('display') display: ElementRef;
+  private viewHeight: number = 0;
 
-  // @ViewChild('wrapper') private wrapper: ElementRef;
-
-  public ngAfterViewInit(): void {
-    /* Scale image/mask */
-    // console.log(this.wrapper);
-    // let bb = this.wrapper.nativeElement.getBoundingClientRect();
+  public ngAfterViewChecked(): void {
+    // only set once
+    if (!this.viewHeight) {
+      setTimeout(() => this.viewHeight = this.display.nativeElement.offsetHeight, 0);
+    }
   }
   protected onPan(event: any): void {
     this.mask.dimensions.x = event.x;
